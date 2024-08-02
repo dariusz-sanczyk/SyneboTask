6d00de292d8842c1b9b98db940eb0b0d5d0e66c9
Darek Sańczyk <tantyk@gmail.com>
2024-08-02 16:24:30 +0200

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        title: 'todo',
+        loadComponent: () =>
+          import('./components/todo/todo.component').then(
+            (m) => m.TodoComponent
+          ),
+      },
+      { path: '**', redirectTo: '/' },
+];
