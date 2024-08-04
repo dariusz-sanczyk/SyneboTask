0e83aa155c51edf5c623b0fae83fef62e9857e3d
Darek Sańczyk <tantyk@gmail.com>
2024-08-04 12:46:23 +0200

    some work on header and input styles

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.scss'
 })
 export class TodoComponent {
+
+  todos = [
+    { description: "EHEHEEHEHEHE" },
+    { description: "AHAHAHAHAH" }
+  ]
 
 }
